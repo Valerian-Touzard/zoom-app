065638527fa8726efc67924be133514413bbd8b9
vtouzard <vtouzard244@gmail.com>
2024-04-03 00:49:58 +0200

    feat: Ajout des icones à coté des liens dans la Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { sidebarLinks } from "@/constants";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 const Sidebar = () => {
   const pathName = usePathname();
@@ -23,7 +24,8 @@ const Sidebar = () => {
                 'bg-blue-1': isActive,
             })}
           >
-            {link.label}
+            <Image src={link.imgUrl} alt={link.label} width={24} height={24} />
+            <p className="text-lg font-semibold max-lg:hidden">{link.label}</p>
           </Link>;
         })}
       </div>
