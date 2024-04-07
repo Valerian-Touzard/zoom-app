01500c39f144efe0cfaee01c6abe56c64ea2d857
vtouzard <vtouzard244@gmail.com>
2024-04-08 00:42:06 +0200

    feat: Ajout du texte pour la prochaine réunion + la date du jour

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <section className='flex size-full flex-col gap-10 text-white'>
-      <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
-
+    <section className="flex size-full flex-col gap-10 text-white">
+      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
+            Prochaine réunion: 12:30 PM
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:tx-7xl">
+              11:30 AM
+            </h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">Lundi 08 Avril, 2024</p>
+          </div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
